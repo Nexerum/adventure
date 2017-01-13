2ecbd9e5283177dbb46624d145cc5754a9f04b11
Nexerum <timhoglund@DAT-1584.local>
2017-01-13 09:56:45 +0200

    Begining
    ändring från nicklas

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
  */
 ?>
 
-<h1>Äventyret</h1>
+<h1>The begining</h1>
 
-<?php
+<?
+//kollar om spelaren har ett namn
 if ($_GET['player_name'] == NULL):
 ?>
-<form action="adventure_mall.php">
-	<label>Vad heter du?</label>
+<form action="index.php">
+	<label>You wake up in a dark room uable to remember any of the events leading up to this point. Can you even rember your name?</label>
 	<input type="text" name="player_name">
 	<input type="hidden" name="page" value="1">
 	<input type="submit" value="Skicka">
@@ -22,16 +23,17 @@ if ($_GET['player_name'] == NULL):
 <?php
 elseif ($_GET['page'] == 1):
 ?>
-<h2>Hej <? echo $_GET['player_name'] ?></h2>
-<p>Du står i en skogsdunge. Till väster ser du en liten stuga med en rykande skorsten och österfrån rinner en liten flod. Norrut ser du bara träd.</p>
-<form action="adventure_mall.php">
-	<label>Åt vilket håll går du?</label><br>
-	<input type="radio" name="page" value="2" id="west">
-	<label for="west">Väster</label><br>
-	<input type="radio" name="page" value="3" id="north">
-	<label for="north">Norr</label><br>
-	<input type="radio" name="page" value="4" id="east">
-	<label for="east">Öster</label><br>
+<p>It seems like the room you're in is some kind of basement. On your left there's some kind of drawer, behind you a tiny window lighting up the room, on the right a sofa and in front of you a wooden door.</p>
+<form action="index.php">
+	<label>What do you do?</label><br>
+	<input type="radio" name="page" value="2" id="drawer">
+	<label for="west">Inspect the drawer.</label><br>
+	<input type="radio" name="page" value="3" id="window">
+	<label for="north">Look out the window.</label><br>
+	<input type="radio" name="page" value="4" id="sofa">
+	<label for="east">Sit on the sofa.</label><br>
+	<input type="radio" name="page" value="5" id="door">
+	<label for="east">Open the door.</label><br>
 	<input type="hidden" name="player_name" value="<?= $_GET['player_name'] ?>">
 	<input type="submit" value="Skicka">
 </form>
@@ -40,18 +42,40 @@ elseif ($_GET['page'] == 1):
 elseif ($_GET['page'] == 2):
 ?>
 <h2>Stugan</h2>
-<p>Det ser inte ut som att någon är hemma.</p>
+<p></p>
 <img src="img/hut.jpg">
-<form action="adventure_mall.php">
-	<label>Åt vilket håll går du?</label><br>
-	<input type="radio" name="page" value="5" id="west">
-	<label for="west">Väster</label><br>
-	<input type="radio" name="page" value="6" id="north">
-	<label for="north">Norr</label><br>
-	<input type="radio" name="page" value="7" id="east">
-	<label for="east">Öster</label><br>
-	<input type="hidden" name="player_name" value="<?= $_GET['player_name'] ?>">
-	<input type="submit" value="Skicka">
+<form action="index.php">
+	
+</form>
+
+<?php
+elseif ($_GET['page'] == 3):
+?>
+<h2>Stugan</h2>
+<p></p>
+<img src="img/hut.jpg">
+<form action="index.php">
+	
+</form>
+
+<?php
+elseif ($_GET['page'] == 4):
+?>
+<h2>Stugan</h2>
+<p></p>
+<img src="img/hut.jpg">
+<form action="index.php">
+	
+</form>
+
+<?php
+elseif ($_GET['page'] == 5):
+?>
+<h2>Stugan</h2>
+<p></p>
+<img src="img/hut.jpg">
+<form action="index.php">
+	
 </form>
 
 <?php
